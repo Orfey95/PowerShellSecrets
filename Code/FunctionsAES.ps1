436598450361808370d880f8799bdeab4131172f
Sasha <sasha7692@gmail.com>
2019-11-29 09:10:53 +0200

    Add files via upload

---
+++ Code/FunctionsAES.ps1
@@ -0,0 +1,19 @@
+﻿# Function to get secret key
+function Get-CryptographyKey {
+       # Create Key 
+       $Global:Key = New-CryptographyKey -Algorithm AES 
+       $Global:Key = ConvertFrom-SecureString -securestring $Global:Key
+       $Global:Key | Out-File $Global:PathToKey
+       echo "`nYou have successfully created AES key!"
+}
+# Function to encrypt the secret file
+function Protect-SecretFile {
+       $Global:Key = (Get-Content $Global:PathToKey | ConvertTo-SecureString)
+       Protect-File -FileName $Global:PathToSecretFile -Key $Global:Key -Algorithm AES -CipherMode CBC -RemoveSource
+}
+# Function to decrypt the secret file
+function Unprotect-SecretFile {
+       [string]$AESFilePath = $Global:PathToSecretFile + '.AES'
+       $Global:Key = (Get-Content $Global:PathToKey | ConvertTo-SecureString)
+       Unprotect-File -FileName $AESFilePath -Key $Global:Key -Algorithm AES -CipherMode CBC -RemoveSource
+}
